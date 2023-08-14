0c3dbe7d48705afc91862571f1771b7b42732ca1
LuisProDev <emailparacontato1000@gmail.com>
2023-08-14 08:46:53 -0300

    botão previsão conectado a função

--- janela_principal.py
+++ janela_principal.py
@@ -1,5 +1,4 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtGui import QKeyEvent
 from janela_secundaria import Ui_OtherWindow
 import datetime
 import keys
@@ -43,20 +42,20 @@ class Ui_MainWindow(object):
         self.local_button.setGeometry(QtCore.QRect(20, 440, 141, 41))
         self.local_button.setMinimumSize(QtCore.QSize(10, 0))
         self.local_button.setStyleSheet("QPushButton{\n"
-"    background-color: rgb(50, 50, 50);\n"
-"    border: 2px solid rgb(60, 60, 60);\n"
-"    border-radius: 5px;\n"
-"    text-align: center;\n"
-"\n"
-"    color: rgb(188, 188, 188);\n"
-"}\n"
-"QPushButton:hover{\n"
-"    border: 2px solid rgb(77, 77, 77);\n"
-"}\n"
-"QPushButton:pressed{\n"
-"    background-color: rgb(117, 117, 117);\n"
-"}\n"
-"")
+                                        "    background-color: rgb(50, 50, 50);\n"
+                                        "    border: 2px solid rgb(60, 60, 60);\n"
+                                        "    border-radius: 5px;\n"
+                                        "    text-align: center;\n"
+                                        "\n"
+                                        "    color: rgb(188, 188, 188);\n"
+                                        "}\n"
+                                        "QPushButton:hover{\n"
+                                        "    border: 2px solid rgb(77, 77, 77);\n"
+                                        "}\n"
+                                        "QPushButton:pressed{\n"
+                                        "    background-color: rgb(117, 117, 117);\n"
+                                        "}\n"
+                                        "")
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(":/logo/pngwing.com.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.local_button.setIcon(icon)
@@ -67,8 +66,8 @@ class Ui_MainWindow(object):
         self.info_frame.setMaximumSize(QtCore.QSize(210, 200))
         self.info_frame.setAutoFillBackground(False)
         self.info_frame.setStyleSheet("QFrame{\n"
-"    background-color: rgb(44, 44, 44);\n"
-"}")
+                                        "    background-color: rgb(44, 44, 44);\n"
+                                        "}")
         self.info_frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.info_frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.info_frame.setObjectName("info_frame")
@@ -97,7 +96,9 @@ class Ui_MainWindow(object):
         self.temp_icon.setGeometry(QtCore.QRect(10, 20, 41, 31))
         self.temp_icon.setStyleSheet("image: url(:/logo/temp.png);")
         self.temp_icon.setText("")
-        self.temp_icon.setPixmap(QtGui.QPixmap(":/logo/kisspng-temperature-thermometer-computer-icons-clip-art-temperature-png-transparent-images-5aae92a8a3c7e9.9723450715213902486709.png"))
+        self.temp_icon.setPixmap(QtGui.QPixmap(":/logo/kisspng-temperature-thermometer-computer"
+                                               "-icons-clip-art-temperature-png-transparent-"
+                                               "images-5aae92a8a3c7e9.9723450715213902486709.png"))
         self.temp_icon.setScaledContents(True)
         self.temp_icon.setObjectName("temp_icon")
 
@@ -108,15 +109,15 @@ class Ui_MainWindow(object):
         self.latitude_entry.setFont(font)
         self.latitude_entry.setAutoFillBackground(False)
         self.latitude_entry.setStyleSheet("QLineEdit{\n"
-"    border: 2px solid rgb(45, 45, 45);\n"
-"    border-radius: 5px;\n"
-"    padding: 10px;\n"
-"    background-color: rgb(30, 30, 30);\n"
-"    color: rgb(188, 188, 188);\n"
-"}\n"
-"QLineEdit:hover{\n"
-"    border: 2px solid rgb(55, 55, 55);\n"
-"}")
+                                            "    border: 2px solid rgb(45, 45, 45);\n"
+                                            "    border-radius: 5px;\n"
+                                            "    padding: 10px;\n"
+                                            "    background-color: rgb(30, 30, 30);\n"
+                                            "    color: rgb(188, 188, 188);\n"
+                                            "}\n"
+                                            "QLineEdit:hover{\n"
+                                            "    border: 2px solid rgb(55, 55, 55);\n"
+                                            "}")
         self.latitude_entry.setMaxLength(32)
         self.latitude_entry.setObjectName("latitude_entry")
 
@@ -127,16 +128,16 @@ class Ui_MainWindow(object):
         self.longitude_entry.setFont(font)
         self.longitude_entry.setAutoFillBackground(False)
         self.longitude_entry.setStyleSheet("QLineEdit{\n"
-"    border: 2px solid rgb(45, 45, 45);\n"
-"    border-radius: 5px;\n"
-"    padding: 10px;\n"
-"    background-color: rgb(30, 30, 30);\n"
-"\n"
-"    color: rgb(188, 188, 188);\n"
-"}\n"
-"QLineEdit:hover{\n"
-"    border: 2px solid rgb(55, 55, 55);\n"
-"}")
+                                            "    border: 2px solid rgb(45, 45, 45);\n"
+                                            "    border-radius: 5px;\n"
+                                            "    padding: 10px;\n"
+                                            "    background-color: rgb(30, 30, 30);\n"
+                                            "\n"
+                                            "    color: rgb(188, 188, 188);\n"
+                                            "}\n"
+                                            "QLineEdit:hover{\n"
+                                            "    border: 2px solid rgb(55, 55, 55);\n"
+                                            "}")
         self.longitude_entry.setMaxLength(32)
         self.longitude_entry.setObjectName("longitude_entry")
 
@@ -146,20 +147,20 @@ class Ui_MainWindow(object):
         self.voltar_button.setMinimumSize(QtCore.QSize(50, 50))
         self.voltar_button.setMaximumSize(QtCore.QSize(100, 100))
         self.voltar_button.setStyleSheet("QPushButton{\n"
-"    background-color: rgb(50, 50, 50);\n"
-"    border: 2px solid rgb(60, 60, 60);\n"
-"    border-radius: 5px;\n"
-"    text-align: center;\n"
-"    \n"
-"    image: url(:/logo/setaesquerda.png);\n"
-"    color: rgb(188, 188, 188);\n"
-"}\n"
-"QPushButton:hover{\n"
-"    border: 2px solid rgb(77, 77, 77);\n"
-"}\n"
-"QPushButton:pressed{\n"
-"    background-color: rgb(117, 117, 117);\n"
-"}")
+                                            "    background-color: rgb(50, 50, 50);\n"
+                                            "    border: 2px solid rgb(60, 60, 60);\n"
+                                            "    border-radius: 5px;\n"
+                                            "    text-align: center;\n"
+                                            "    \n"
+                                            "    image: url(:/logo/setaesquerda.png);\n"
+                                            "    color: rgb(188, 188, 188);\n"
+                                            "}\n"
+                                            "QPushButton:hover{\n"
+                                            "    border: 2px solid rgb(77, 77, 77);\n"
+                                            "}\n"
+                                            "QPushButton:pressed{\n"
+                                            "    background-color: rgb(117, 117, 117);\n"
+                                            "}")
         self.voltar_button.setText("")
         self.voltar_button.setAutoDefault(False)
         self.voltar_button.setDefault(False)
@@ -171,39 +172,39 @@ class Ui_MainWindow(object):
         self.seta_direita.setMinimumSize(QtCore.QSize(50, 50))
         self.seta_direita.setMaximumSize(QtCore.QSize(100, 100))
         self.seta_direita.setStyleSheet("QPushButton{\n"
-"    background-color: rgb(50, 50, 50);\n"
-"    border: 2px solid rgb(60, 60, 60);\n"
-"    border-radius: 5px;\n"
-"    text-align: center;\n"
-"    image: url(:/logo/setadireita.png);\n"
-"    color: rgb(188, 188, 188);\n"
-"}\n"
-"QPushButton:hover{\n"
-"    border: 2px solid rgb(77, 77, 77);\n"
-"}\n"
-"QPushButton:pressed{\n"
-"    background-color: rgb(117, 117, 117);\n"
-"}")
+                                        "    background-color: rgb(50, 50, 50);\n"
+                                        "    border: 2px solid rgb(60, 60, 60);\n"
+                                        "    border-radius: 5px;\n"
+                                        "    text-align: center;\n"
+                                        "    image: url(:/logo/setadireita.png);\n"
+                                        "    color: rgb(188, 188, 188);\n"
+                                        "}\n"
+                                        "QPushButton:hover{\n"
+                                        "    border: 2px solid rgb(77, 77, 77);\n"
+                                        "}\n"
+                                        "QPushButton:pressed{\n"
+                                        "    background-color: rgb(117, 117, 117);\n"
+                                        "}")
         self.seta_direita.setText("")
         self.seta_direita.setObjectName("seta_direita")
 
         self.botao_previsao = QtWidgets.QPushButton(self.fundo_frame)
         self.botao_previsao.setGeometry(QtCore.QRect(440, 400, 101, 31))
         self.botao_previsao.setStyleSheet("QPushButton{\n"
-"    background-color: rgb(50, 50, 50);\n"
-"    border: 2px solid rgb(60, 60, 60);\n"
-"    border-radius: 5px;\n"
-"    text-align: center;\n"
-"\n"
-"    color: rgb(188, 188, 188);\n"
-"}\n"
-"QPushButton:hover{\n"
-"    border: 2px solid rgb(77, 77, 77);\n"
-"}\n"
-"QPushButton:pressed{\n"
-"    background-color: rgb(117, 117, 117);\n"
-"}\n"
-"")
+                                            "    background-color: rgb(50, 50, 50);\n"
+                                            "    border: 2px solid rgb(60, 60, 60);\n"
+                                            "    border-radius: 5px;\n"
+                                            "    text-align: center;\n"
+                                            "\n"
+                                            "    color: rgb(188, 188, 188);\n"
+                                            "}\n"
+                                            "QPushButton:hover{\n"
+                                            "    border: 2px solid rgb(77, 77, 77);\n"
+                                            "}\n"
+                                            "QPushButton:pressed{\n"
+                                            "    background-color: rgb(117, 117, 117);\n"
+                                            "}\n"
+                                            "")
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap(":/logo/botao-play.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.botao_previsao.setIcon(icon1)
@@ -219,6 +220,7 @@ class Ui_MainWindow(object):
         # Conexões
         self.local_button.clicked.connect(self.abrir_janela)
         self.seta_direita.clicked.connect(self.comando_direito)
+        self.botao_previsao.clicked.connect(self.weather_system)
         ui.ui_secundaria.botao_copiar.clicked.connect(self.copiar_para_previsao)
         ui.ui_secundaria.buscar_button.clicked.connect(self.buscar_lat_lon)
 
@@ -245,17 +247,19 @@ class Ui_MainWindow(object):
         self.user_long = geo_data[0]["lon"]
 
         ui.ui_secundaria.lat_label.setText(f"Lat:{self.user_lat}")
-        ui.ui_secundaria.lat_label.setText(_translate("OtherWindow","<html><head/><body><p><span style=\""
-                                                                    " font-size:12pt; font-weight:700; "
-                                                                    f"color:#797979;\">Lat:{self.user_lat}"
-                                                                    f"</span></p></body></html>"))
+        ui.ui_secundaria.lat_label.setText(_translate("OtherWindow",
+                                                      "<html><head/><body><p><span style=\""
+                                                        " font-size:12pt; font-weight:700; "
+                                                        f"color:#797979;\">Lat:{self.user_lat}"
+                                                        f"</span></p></body></html>"))
         ui.ui_secundaria.lat_label.setGeometry(QtCore.QRect(230, 180, 150, 31))
 
         ui.ui_secundaria.long_label.setText(f"Lon:{self.user_long}")
-        ui.ui_secundaria.long_label.setText(_translate("OtherWindow", "<html><head/><body><p><span style=\""
-                                                                      " font-size:12pt; font-weight:700;"
-                                                                      f" color:#797979;\">Lon:{self.user_long}"
-                                                                      "</span></p></body></html>"))
+        ui.ui_secundaria.long_label.setText(_translate("OtherWindow",
+                                                       "<html><head/><body><p><span style=\""
+                                                      " font-size:12pt; font-weight:700;"
+                                                      f" color:#797979;\">Lon:{self.user_long}"
+                                                      "</span></p></body></html>"))
         ui.ui_secundaria.long_label.setGeometry(QtCore.QRect(230, 210, 150, 31))
 
         print(self.user_lat, self.user_long)
@@ -267,7 +271,8 @@ class Ui_MainWindow(object):
         data_formatada = self.data_atual.strftime('%d/%m')
         self.dia.setText(_translate("MainWindow", "<html><head/><body><p><span style=\""
                                                   " font-size:12pt; font-weight:700; "
-                                                  f"color:#797979;\">{data_formatada}</span></p></body></html>"))
+                                                  f"color:#797979;\">{data_formatada}"
+                                                  f"</span></p></body></html>"))
 
     def comando_esquerdo(self):
         _translate = QtCore.QCoreApplication.translate
@@ -275,14 +280,18 @@ class Ui_MainWindow(object):
         data_formatada = self.data_atual.strftime('%d/%m')
         self.dia.setText(_translate("MainWindow", "<html><head/><body><p><span style=\""
                                                   " font-size:12pt; font-weight:700; "
-                                                  f"color:#797979;\">{data_formatada}</span></p></body></html>"))
+                                                  f"color:#797979;\">{data_formatada}"
+                                                  f"</span></p></body></html>"))
 
 
     def weather_system(self):
+        user_lat = self.latitude_entry.text()
+        user_long = self.longitude_entry.text()
+
         weather_key = keys.weather_key
         wheather_endp = "http://api.weatherapi.com/v1/forecast.json"
         wheather_param = {
-            "q": (self.user_lat, self.user_long),
+            "q": (user_lat, user_long),
             'key': weather_key,
             "days": 7,
             "hour": 16
@@ -292,12 +301,11 @@ class Ui_MainWindow(object):
         weather_connection.raise_for_status()
         weather_data = weather_connection.json()
 
+        print("ok")
+
         self.seven_days = []
         for i in range(0, 7):
             self.seven_days.append(weather_data['forecast']['forecastday'][i]['day']['condition'])
-
-
-
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
